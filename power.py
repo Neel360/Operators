0d73330344214beed25d99ccdd8492da99d4eabb
neel-guckool <neel001g@gmail.com>
2026-05-26 10:03:27 +0100

    Operators

---
+++ power.py
@@ -0,0 +1,6 @@
+print("This program calculates the power of a number\n2")
+
+base = int(input("Enter the base: "))
+expontent = int(input("Enter the exponent: "))
+
+print(f"The result of {base} to the power of {expontent} is {base**expontent}")
